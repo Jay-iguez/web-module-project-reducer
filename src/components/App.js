a33d5ea3dbf0391a8eb09ae57621cf277e224e83
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-09-04 15:14:48 -0700

    Initialize imports from reducers folder and useReducer logic in AppJS

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
+import {reducer, initialState} from '../reducers/index'
 
 function App() {
+  const [calculatorValue, calculatorValueDispatch] = useReducer(reducer, initialState)
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
